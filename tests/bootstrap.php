885e517b4ec68832f2bd0a362440d7a475c73bd5
Dave Widmer <dwidmer@bgsu.edu>
2012-06-13 13:08:47 -0400

    Moving over to the Composer autoloader.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,15 +1,7 @@
 <?php
 
 // Setup the autoloader
-include __DIR__.DIRECTORY_SEPARATOR."classes".DIRECTORY_SEPARATOR."SplClassLoader.php";
-
-$parts = array(__DIR__, "..", "classes");
-$path = realpath(implode(DIRECTORY_SEPARATOR, $parts)).DIRECTORY_SEPARATOR;
-
-$autoload = new SplClassLoader(null, $path);
-$autoload->register();
-
-unset($parts, $path);
+include dirname(__DIR__).DIRECTORY_SEPARATOR."vendor".DIRECTORY_SEPARATOR."autoload.php";
 
 // Load up clases needed for the tests
 foreach (glob("tests/classes/*.php") as $filename)
